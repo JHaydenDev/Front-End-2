17d01f3c105d69138c59080e0582b9fbf75f8291
chrismjohnston <cmjohnston@gmail.com>
2019-10-21 19:06:42 -0500

    basic nav framework built

---
+++ src/components/Nav.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { Link } from 'react-router-dom';
+
+
+// Navigation bar components
+const Nav = props => {
+    <header className="navbar_header">
+        <nav className="navbar_items">
+            <div>
+                <img src="">Logo</img>
+            </div>
+            <Link to="">Home</Link>
+            <Link to="">Start Game</Link>
+            <Link to="">Login/Sign Up</Link>
+        </nav>
+    </header>
+
+
+}
+
+export default Nav;
