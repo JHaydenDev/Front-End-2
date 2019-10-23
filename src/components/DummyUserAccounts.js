dd2e21e8a4cf86e561e9158b7596631747c0bf98
ngaivoronski <ngaivoronski@gmail.com>
2019-10-23 19:13:50 -0400

    added dummy user accounts

---
+++ src/components/DummyUserAccounts.js
@@ -0,0 +1,32 @@
+export default [
+    {
+        id: 1,
+        username: "Bob123",
+        password: "password123",
+        globalPoints: 5,
+    },
+    {
+        id: 2,
+        username: "DevelopersDevelopers",
+        password: "IUseBing",
+        globalPoints: 0,
+    },
+    {
+        id: 3,
+        username: "JasonJSON",
+        password: "pronouncedJaySon",
+        globalPoints: 3,
+    },
+    {
+        id: 4,
+        username: "REACTologist",
+        password: "PasswordComponent",
+        globalPoints: 0,
+    },
+    {
+        id: 5,
+        username: "HerokuShima",
+        password: "GETrequestDenied",
+        globalPoints: 5,
+    },
+];
