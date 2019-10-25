4eebf1a078d22e04a958a09fd0c3045256d7cf00
ngaivoronski <ngaivoronski@gmail.com>
2019-10-24 20:42:09 -0400

    updated login forms and added account deletion functionality

--- src/components/Login.js
+++ src/components/Login.js
@@ -8,6 +8,7 @@ class Login extends React.Component {
       username: "",
       password: ""
     },
+    message: "",
   };
 
   handleChange = e => {
@@ -41,8 +42,12 @@ class Login extends React.Component {
             username: info.username,
         })
         console.log(this.props.loggedInUser);
+        this.setState({message:``});
       })
-      .catch(err => console.log(err.response));
+      .catch(err => {
+        console.log(err.response);
+        this.setState({message:`${err.response.data.message}`});
+      });
   };
 
   GoToRegister = () => {
@@ -56,6 +61,7 @@ class Login extends React.Component {
       return (
         <div>
           <form onSubmit={e => this.login(e, this.state.credentials)} className="login-form">
+            <div style={{color: "red"}}>{this.state.message}</div>
             <div className="input-div">
               <input
                 type="text"
