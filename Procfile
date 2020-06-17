3427d133e277096c1624b825d47e4a21b40d577c
jhaydendev <josephhaydendevelopment@gmail.com>
2020-06-16 22:20:53 -0500

    d

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: web: bin/boot
+web: node src/components/myscript.js
---
+++ build/precache-manifest.08218ef734af310c8363038795a1901a.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "425c28db81e59f290e246d2ab16f14f9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "754a6f6299ba93c929bd",
+    "url": "/static/css/main.1c7e9199.chunk.css"
+  },
+  {
+    "revision": "e9a3e5b101b473909c57",
+    "url": "/static/js/2.2b03571f.chunk.js"
+  },
+  {
+    "revision": "754a6f6299ba93c929bd",
+    "url": "/static/js/main.b31d9939.chunk.js"
+  },
+  {
+    "revision": "8886aaf2a58d063c25b9",
+    "url": "/static/js/runtime-main.ceffe203.js"
+  },
+  {
+    "revision": "05772182b3362a8a5ee2822854f17f0a",
+    "url": "/static/media/Amy Klobuchar.05772182.jpg"
+  },
+  {
+    "revision": "07dd6d738f2110fd7cc019ab49a86f90",
+    "url": "/static/media/Andrew Yang.07dd6d73.jpg"
+  },
+  {
+    "revision": "49a4ca697884f966972b1f9a0787cc6e",
+    "url": "/static/media/Bernie Sanders.49a4ca69.jpg"
+  },
+  {
+    "revision": "45d9858690c7c23398b0f32f13d3ecda",
+    "url": "/static/media/Beto O'Rourke.45d98586.jpg"
+  },
+  {
+    "revision": "b562eff42f0af997496d7d9920e2fbb6",
+    "url": "/static/media/Bill Weld.b562eff4.jpg"
+  },
+  {
+    "revision": "a982fc766d6f8f9ed75586403223191f",
+    "url": "/static/media/Cory Booker.a982fc76.jpg"
+  },
+  {
+    "revision": "07d33478bc5c271a9f338ea346649866",
+    "url": "/static/media/Donald Trump.07d33478.jpg"
+  },
+  {
+    "revision": "27107270b152ea43911c2609bbdf2c13",
+    "url": "/static/media/Elizabeth Warren.27107270.jpg"
+  },
+  {
+    "revision": "9204fc48b5191403d97e805d598c3ba8",
+    "url": "/static/media/Joe Biden.9204fc48.jpg"
+  },
+  {
+    "revision": "bd6a52fa874cb761ec5745c621b06772",
+    "url": "/static/media/Joe Sestak.bd6a52fa.jpg"
+  },
+  {
+    "revision": "95b58693696a0dc1f97fe3c43eee6b99",
+    "url": "/static/media/Joe Walsh.95b58693.jpg"
+  },
+  {
+    "revision": "1a9c269df0e90f84ff76ade0bc456590",
+    "url": "/static/media/John Delaney.1a9c269d.jpg"
+  },
+  {
+    "revision": "888849f2af19c00ddac09a33e2b51016",
+    "url": "/static/media/Julian Castro.888849f2.jpg"
+  },
+  {
+    "revision": "7d428beca354d6db5c8d19fab2daedb3",
+    "url": "/static/media/Kamala Harris.7d428bec.jpg"
+  },
+  {
+    "revision": "f1556e02cc9d710965dcbc3a1a10ea21",
+    "url": "/static/media/Marianne Williamson.f1556e02.jpg"
+  },
+  {
+    "revision": "dde77099af73110b037b4bf403767e61",
+    "url": "/static/media/Mark Sanford.dde77099.jpg"
+  },
+  {
+    "revision": "e4f5acbac1a2af77533ddeaf10ab6d0c",
+    "url": "/static/media/Michael Bennet.e4f5acba.jpg"
+  },
+  {
+    "revision": "490425ea0f5d5870f49c93ad4c94fe88",
+    "url": "/static/media/Pete Buttigieg.490425ea.jpg"
+  },
+  {
+    "revision": "aedada04e143e427329d31f4823d64ef",
+    "url": "/static/media/Steve Bullock.aedada04.jpg"
+  },
+  {
+    "revision": "c39c38a4c02b3e799b1fbc572d890b65",
+    "url": "/static/media/Tim Ryan.c39c38a4.jpg"
+  },
+  {
+    "revision": "57696077a2a0145b768605e9379e2c93",
+    "url": "/static/media/Tom Steyer.57696077.jpg"
+  },
+  {
+    "revision": "897a861a200b182458a73c41e4a2104e",
+    "url": "/static/media/Tulsi Gabbard.897a861a.jpg"
+  },
+  {
+    "revision": "c6324aff97e428fa4b3e772d07a2e206",
+    "url": "/static/media/Wayne Messam.c6324aff.jpg"
+  }
+]);
